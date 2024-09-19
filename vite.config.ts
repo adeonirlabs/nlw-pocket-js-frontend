58c3716bce4fab912af2aa498d90d7436f638a7e
Adeonir Kohl <adeonir@gmail.com>
2024-09-19 19:53:49 -0300

    Install and setup tanstack/router

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       '~': '/src',
