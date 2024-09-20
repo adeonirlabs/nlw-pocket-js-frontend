ecf5eb8af83010861ce7f8017a36aa4fc1e93297
Adeonir Kohl <adeonir@gmail.com>
2024-09-20 00:29:02 -0300

    Add week summary with goals list

--- src/components/app/week-summary.tsx
+++ src/components/app/week-summary.tsx
@@ -1,8 +1,10 @@
-import { Plus } from 'lucide-react'
+import { CheckCircle2, Plus } from 'lucide-react'
 import { BrandIcon } from '~/components/icons/brand-icon'
 
 import { Button } from '~/components/ui/button'
 import { Dialog } from '~/components/ui/dialog'
+import { ProgressBar } from '~/components/ui/progress-bar'
+import { Separator } from '~/components/ui/separator'
 
 export function WeekSummary() {
   return (
@@ -15,6 +17,52 @@ export function WeekSummary() {
             <Plus className="size-4" /> Cadastrar meta
           </Button>
         </Dialog.Trigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <ProgressBar value={10} max={15}>
+          <ProgressBar.Indicator style={{ width: `${(10 / 15) * 100}%` }} />
+        </ProgressBar>
+        <div className="flex items-center justify-between">
+          <span className="text-sm text-zinc-500">10/15</span>
+          <span className="text-sm text-zinc-500">100%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="flex flex-wrap gap-3">
+        {Array.from({ length: 5 }, (_, index) => (
+          <Button key={index.toString()} variant="outline">
+            <Plus className="size-4 text-zinc-500" />
+            Meta {index + 1}
+          </Button>
+        ))}
+      </div>
+
+      <Separator />
+
+      <div className="flex flex-col gap-6">
+        <h2 className="font-medium text-xl">Sua semana</h2>
+        <div className="flex flex-col gap-4">
+          <h3 className="flex items-baseline gap-1 font-medium">
+            <span>Segunda-feira</span>
+            <span className="text-xs text-zinc-400">(19 de Maio)</span>
+          </h3>
+          <ul className="flex flex-col gap-3">
+            {Array.from({ length: 5 }, (_, index) => (
+              <li className="flex items-center gap-2" key={index.toString()}>
+                <CheckCircle2 className="size-4 text-pink-500" />
+                <span className="text-sm text-zinc-400">
+                  {'  Você completou '}
+                  <span className="text-zinc-100">Meta {index + 1}</span>
+                  {' às '}
+                  <span className="text-zinc-100">8:13</span>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 import { CreateGoal } from '~/components/app/create-goal'
-import { EmptyGoals } from '~/components/app/empty-goals'
+// import { EmptyGoals } from '~/components/app/empty-goals'
+import { WeekSummary } from '~/components/app/week-summary'
 import { Dialog } from '~/components/ui/dialog'
 
 export const Route = createFileRoute('/')({
@@ -11,7 +12,8 @@ export const Route = createFileRoute('/')({
 function Index() {
   return (
     <Dialog>
-      <EmptyGoals />
+      {/* <EmptyGoals /> */}
+      <WeekSummary />
       <CreateGoal />
     </Dialog>
   )
