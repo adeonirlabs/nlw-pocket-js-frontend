7d59d3166e1f94e66d7baf10dd0a951a25a2bddf
Adeonir Kohl <adeonir@gmail.com>
2024-09-21 20:09:14 -0300

    Install zod and create env validation

--- src/env.ts
+++ src/env.ts
@@ -4,4 +4,4 @@ const envSchema = z.object({
   API_URL: z.string().url().default('http://localhost:3333'),
 })
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(import.meta.env)
