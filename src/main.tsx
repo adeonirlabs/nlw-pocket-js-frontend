36f2996001841771375d979d5f34b7a5a62dd8db
Adeonir Kohl <adeonir@gmail.com>
2024-09-21 19:56:49 -0300

    Install and setup tanstack/query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { queryClient } from '~/lib/query-client'
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
@@ -19,7 +21,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   )
 }
