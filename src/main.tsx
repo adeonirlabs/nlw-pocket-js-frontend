660bfe696abea1354b1c2b358551ddfd88e15c33
Adeonir Kohl <adeonir@gmail.com>
2024-09-22 08:18:12 -0300

    Install and setup tanstack/query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import './index.css'
 
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
@@ -23,6 +24,7 @@ if (!rootElement.innerHTML) {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </StrictMode>
   )
